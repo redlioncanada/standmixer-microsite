87d9ed48d8e36fa6e9f82c2a767768920cc375e8
Stephen Poole <stephen.poole@redlioncanada.com>
2015-06-12 16:54:58 -0400

    Initial commit. Finished landing page layout, started second page.

---
+++ debug/js/standmixer/main.js
@@ -0,0 +1,64 @@
+"use strict";
+
+var isMobile = Modernizr.mobile;
+var isPhone = Modernizr.phone;
+var isTablet = Modernizr.tablet;
+
+if (isMobile) {
+    //inject meta tags
+    $("head").append("<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' name='viewport' />").append("<meta content='True' name='HandheldFriendly' />");
+}
+
+$(document).ready(function () {
+    //start first panel mixer change
+    var mixerTimeout = undefined;
+    resetMixerInterval();
+    function resetMixerInterval() {
+        clearTimeout(mixerTimeout);
+        mixerTimeout = setInterval(function () {
+            changeMixer();
+        }, 5000);
+    }
+
+    $(".categories").on("click", "li:not(\".selected,.title\")", function () {
+        var id = $(this).index();
+        resetMixerInterval();
+        changeMixer(id);
+    });
+
+    function changeMixer(id) {
+        if (typeof id === "undefined") {
+            id = $(".categories .selected").index() + 1;
+            if (id > $(".categories li").length - 1) id = 1;
+        }
+
+        var target = $(".categories li").eq(id);
+
+        //change mixer image
+        $(".mixer .selected").fadeOut(500);
+        $(".mixer img").eq(id - 1).fadeIn(400, function () {
+            $(this).addClass("selected");
+        });
+
+        //change menu selection
+        $(".categories .selected").removeClass("selected");
+        $(target).addClass("selected");
+    }
+    //end first panel mixer change
+
+    //on load complete, hide overlay
+    Pace.on("done", function () {
+        $("#loading").fadeOut();
+    });
+
+    //on click-to-top click, go to top
+    $(".back-to-top").click(function () {
+        $("body").animate({ scrollTop: 0 }, "400");
+    });
+
+    /*redraw();
+    //on window resize, resize components
+    $(window).resize(redraw);
+    function redraw() {
+    }*/
+});
